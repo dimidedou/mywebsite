a4f81cd0758dc95cedf9bf6572785356675ae03f
Dimitris-Dedousis <72694765+dimidedou@users.noreply.github.com>
2024-07-03 19:27:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-const firebaseConfig={apiKey:"AIzaSyAy49lJTcgXi4M_OVF8ccz6UPldjo7wnEU",authDomain:"my-form-b4967.firebaseapp.com",projectId:"my-form-b4967",storageBucket:"my-form-b4967.appspot.com",messagingSenderId:"510396488382",appId:"1:510396488382:web:0b81b68ea6aa5ad026dcef"};firebase["initializeApp"](firebaseConfig);const contactForm=document["getElementById"]('contactForm');contactForm["addEventListener"]('submit',async (_0xe316x2)=>{_0xe316x2["preventDefault"]();const _0xe316x3= new FormData(contactForm);const _0xe316x4=_0xe316x3["get"]('name');const _0xe316x5=_0xe316x3["get"]('email');const _0xe316x6=_0xe316x3["get"]('message');const _0xe316x7=firebase["firestore"](); await _0xe316x7["collection"]('messages')["add"]({name:_0xe316x4,email:_0xe316x5,message:_0xe316x6,timestamp:firebase["firestore"]["FieldValue"]["serverTimestamp"]()});contactForm["reset"]();alert('\u03a4\u03bf \u03bc\u03ae\u03bd\u03c5\u03bc\u03b1 \u03c3\u03b1\u03c2 \u03b1\u03c0\u03bf\u03b8\u03b7\u03ba\u03b5\u03cd\u03c4\u03b7\u03ba\u03b5 \u03bc\u03b5 \u03b5\u03c0\u03b9\u03c4\u03c5\u03c7\u03af\u03b1!')})
+// Φόρτωση του περιεχομένου από το αρχείο my.txt
+fetch('./my.txt')
+  .then(response => response.text())
+  .then(apiKey => {
+    // Αρχικοποίηση Firebase με τη χρήση του apiKey από το αρχείο my.txt
+    const firebaseConfig = {
+      apiKey: apiKey.trim(), // Χρήση του apiKey από το αρχείο my.txt
+      authDomain: "my-form-b4967.firebaseapp.com",
+      projectId: "my-form-b4967",
+      storageBucket: "my-form-b4967.appspot.com",
+      messagingSenderId: "510396488382",
+      appId: "1:510396488382:web:0b81b68ea6aa5ad026dcef",
+    };
+    
+    firebase.initializeApp(firebaseConfig);
+
+    // Αντιμετώπιση υποβολής της φόρμας
+    const contactForm = document.getElementById('contactForm');
+    contactForm.addEventListener('submit', async (e) => {
+      e.preventDefault(); // Αποτροπή της προεπιλεγμένης συμπεριφοράς φόρμας
+
+      // Συλλογή δεδομένων από τη φόρμα
+      const formData = new FormData(contactForm);
+      const name = formData.get('name');
+      const email = formData.get('email');
+      const message = formData.get('message');
+
+      // Αποθήκευση δεδομένων στη βάση δεδομένων Firebase
+      const db = firebase.firestore();
+      await db.collection('messages').add({
+        name: name,
+        email: email,
+        message: message,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp()
+      });
+
+      // Καθαρισμός της φόρμας μετά την υποβολή
+      contactForm.reset();
+      alert('Το μήνυμα σας αποθηκεύτηκε με επιτυχία!');
+    });
+  })
+  .catch(error => console.error('Σφάλμα φόρτωσης αρχείου my.txt:', error));
